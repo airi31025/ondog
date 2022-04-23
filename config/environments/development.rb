1b614364d7af75ac5bb6c25cf2d2e1c896c451d7
airi31025 <rema.717@i.softbank.jp>
2022-04-23 11:10:55 +0000

    [Add]トップページ作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "deb6085ae18545988bf0906a84966b3b.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
