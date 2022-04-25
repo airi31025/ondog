8da0718d06d79ff782017d1bf170eae10b5365fd
airi31025 <rema.717@i.softbank.jp>
2022-04-25 15:54:49 +0000

    [Add]ヘッダー作成

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,10 +1,24 @@
 class Admin::QuestionsController < ApplicationController
   def new
+    @question = Question.new
+  end
+  
+  def create
+    question = Question.new(question_params)
+    question.save
+    redirect_to admin_question_path
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  def question_params
+    params.require(:question).permit(:title, :question, :answer)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,30 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'pets/index'
-    get 'pets/show'
-  end
-  namespace :admin do
-    get 'questions/new'
-    get 'questions/show'
-    get 'questions/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
+  root to: "homes#top"
+  
+  devise_for :users,skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
   namespace :user do
+    resources :pets,only:[:new,:create,:show,:edit,:update]
+    resources :questions,only:[:index]
+    get 'questions/practice'
+    post 'questions/test'
     get 'answer_results/congrats'
   end
-  namespace :user do
-    get 'questions/index'
-    get 'questions/practice'
-    get 'questions/test'
-  end
-  namespace :user do
-    get 'pets/new'
-    get 'pets/show'
-    get 'pets/edit'
-  end
+  
   devise_for :admin,skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
 
-  devise_for :users,skip: [:passwords],controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-  root to: "homes#top"
+  namespace :admin do
+    get 'homes/top'
+    resources :questions,only:[:new,:create,:show,:edit,:update,:destroy]
+    resources :pets,only:[:index,:show]
+  end
+  
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
