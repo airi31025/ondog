dd9894a834825ae186c5d6c66054553384b1f82a
airi31025 <rema.717@i.softbank.jp>
2022-04-26 08:52:55 +0000

    [Add]新規登録画面作成

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -2,11 +2,11 @@ class Admin::QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
-  
+
   def create
     question = Question.new(question_params)
     question.save
-    redirect_to admin_question_path
+    redirect_to admin_question_path(question.id)
   end
 
   def show
@@ -14,11 +14,24 @@ class Admin::QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
-  
+
+  def update
+    question = Question.find(params[:id])
+    question.update(question_params)
+    redirect_to admin_question_path(question.id)
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    question.destroy
+    redirect_to admin_question_path(question.id)
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :question, :answer)
   end
-  
+
 end
