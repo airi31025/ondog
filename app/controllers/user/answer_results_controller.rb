9f6f95a34eaf2fedfcaa64404e5b0826c712efa0
airi31025 <rema.717@i.softbank.jp>
2022-05-02 13:07:21 +0000

    [Add]テスト画面作成

--- app/controllers/user/answer_results_controller.rb
+++ app/controllers/user/answer_results_controller.rb
@@ -1,4 +1,24 @@
 class User::AnswerResultsController < ApplicationController
+  def create
+    # byebug
+    question = Question.find(params[:question_id])
+    question.question_contents.each_with_index do |content, index|
+    if content.answer == params[:answers]
+       redirect_to user_answer_results_congrats_path
+    else
+      render user_question_practice_path(question.id)
+    end
+
+  end
+end
+
+  #   if  question.question_contents_ansers == question.answer_results
+  #     redirect_to user_answer_results_congrats
+  #   else
+  #     render user_question_practice
+  #   end
+  # end
+
   def congrats
   end
 end
--- app/controllers/user/questions_controller.rb
+++ app/controllers/user/questions_controller.rb
@@ -9,20 +9,11 @@ class User::QuestionsController < ApplicationController
 
   def test
     @question = Question.find(params[:question_id])
-    @question = Question.new
-    @question.answer_results = (0..4).map do |r|
+    @new_question = Question.new
+    @new_question.answer_results = (0..4).map do |r|
       AnswerResult.new
     end
   end
 
-  def create
-    question = Question.new(title: params[:question][:title])
-    if question.save
-      (0..4).each do |index|
-        question.answer_results.create(
-          answer: params[:question][:answer][index.to_s]
-        )
-      end
-    end
-  end
+  
 end
