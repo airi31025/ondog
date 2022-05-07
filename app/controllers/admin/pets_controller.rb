76a25650124f5f563c767650946ee0272d93ad2e
airi31025 <rema.717@i.softbank.jp>
2022-05-07 13:54:26 +0000

    [Add]プログレスバー作成

--- app/controllers/admin/pets_controller.rb
+++ app/controllers/admin/pets_controller.rb
@@ -1,11 +1,11 @@
 class Admin::PetsController < ApplicationController
   def index
     @users = User.all
-    @pet = Pet.all
   end
 
   def show
     @pet = Pet.find(params[:id])
+    @explevel = [0,0,10,21,33,46]
     @user = @pet.user
   end
 end
